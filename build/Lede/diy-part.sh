0cec6a0b81278137835b0e98f35522367be8fdda
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-05 22:51:37 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -62,7 +62,7 @@ export Cancel_running="0"                    # 取消路由器每天跑分任务
 export amlogic_model="s905d"
 export amlogic_kernel="5.10.01_6.1.01"
 export auto_kernel="true"
-export rootfs_size="2560"
+export rootfs_size="512/2560"
 export kernel_usage="stable"
 
 
