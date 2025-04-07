f4953332deb47e8541f55ec0f8899a3d1481f640
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-07 12:18:45 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -78,11 +78,6 @@ sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 
 
-git clone --depth 1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --single-branch --depth=1 --branch=main https://github.com/xiaorouji/openwrt-passwall-packages  package/passwall-packages
-
-
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 packages
